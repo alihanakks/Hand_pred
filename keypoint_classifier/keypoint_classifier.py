757e0a06722ca38f071dbd5defd33f3cf748f951
Alihan Akkus <alihan.akkus@outlook.com>
2024-09-24 20:28:44 +0300

    first commit

---
+++ keypoint_classifier/keypoint_classifier.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+import numpy as np
+
+from tensorflow.keras.models import load_model
+
+class KeyPointClassifier(object):
+    def __init__(
+        self,
+        model_path=(r'C:\Users\aliha\OneDrive\Desktop\el_algılama\keypoint_classifier\model.keras'),
+        ):
+        self.model=load_model(model_path)
+    
+        
+    def __call__(
+        self,
+        data
+        ):
+        self.data=data
+        self.predict=self.model.predict(data)
+        self.best_prediction = np.argmax(self.predict, axis=-1)
+        print(self.predict)
+        return self.best_prediction
+           
+    
+    
+   
---
+++ main.py
@@ -0,0 +1,384 @@
+import cv2
+import mediapipe
+import copy
+import csv
+import itertools
+import numpy as np
+import pandas as pd
+import tensorflow as tf #tensorflow version==2.17.0
+
+from joblib import load
+from keypoint_classifier.keypoint_classifier import KeyPointClassifier 
+
+
+
+def main():
+    #kamera erisim
+    camera=cv2.VideoCapture(0) 
+    #model yukleme  
+    mpHands=mediapipe.solutions.hands
+    hands=mpHands.Hands()
+    model=KeyPointClassifier()
+    #Etiketleri okuma
+    with open('keypoint_classifier/keypoint_classifier_label.csv',
+            encoding='utf-8-sig') as f:
+        keypoint_classifier_labels = csv.reader(f)
+        keypoint_classifier_labels = [
+            row[0] for row in keypoint_classifier_labels ]
+    
+    mode=0
+    use_brect = True  
+    ##############################################################                            
+    while True:
+        
+        #cıkıs icin tuş atama
+        key = cv2.waitKey(10)
+        if key == 27:  # ESC
+            break
+        
+        #Veri kayıdı icin mode secme numara belirmeleme
+        number, mode = select_mode(key, mode)
+        
+        #Kamerada kare yakalama
+        success, image=camera.read()
+        if not success:
+            break
+        image=cv2.flip(image,1)
+        debug_image = copy.deepcopy(image)
+        
+        #Algılama uygulaması
+        imgrgb=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        hlms =hands.process(imgrgb)
+        image.flags.writeable = True     
+        ####################################################################
+        if hlms.multi_hand_landmarks is not None:
+            for hand_landmarks, handedness in zip(hlms.multi_hand_landmarks,
+                                                  hlms.multi_handedness):
+                # SInırlayıcı Kutu Berlirleme
+                brect = calc_bounding_rect(debug_image, hand_landmarks)
+                #Parmak Kordinatları Donusturme                   
+                landmark_list = calc_landmark_list(debug_image, hand_landmarks)
+                #Ham veriyi csv olarak kayıt etme    
+                logging_csv(number,mode,Veri_alma(landmark_list))
+                #veri on isleme
+                data=Veri_alma(landmark_list,scaler=True)
+                #modele Gönderilmesi
+                hand_sign_id=model(data)
+                #Tahminin sınıflandırılıması
+                Predict=el_tahmin(hand_sign_id,keypoint_classifier_labels)                
+                #Cizim asaması
+                debug_image = draw_bounding_rect(use_brect, debug_image, brect)
+                debug_image = draw_landmarks(debug_image, landmark_list)
+                debug_image=draw_info_text(debug_image, brect, handedness,Predict)
+        
+        cv2.imshow("kamera",debug_image)
+
+#Modelden gelen tahminin hangi sınıfa ait oldugunu bulma        
+def el_tahmin(best_pred,KeyPoint_label):
+    for i in range (len(KeyPoint_label)):
+        print(best_pred,i)
+        
+        if best_pred==i+1:
+            print(best_pred,i)
+            return KeyPoint_label[i]
+        
+def select_mode(key, mode):
+    number = -1
+    if 48 <= key <= 57:  # 0 ~ 9
+        number = key - 48
+    if key == 110:  # n
+        mode = 0
+    if key == 107:  # k
+        mode = 1
+    return number, mode
+
+#parmak kordinatları Donusturme
+def calc_landmark_list(image, landmarks):
+    image_width, image_height = image.shape[1], image.shape[0]
+
+    landmark_point = []
+
+    # Anahtar_nokta
+    for _, landmark in enumerate(landmarks.landmark):
+        landmark_x = min(int(landmark.x * image_width), image_width - 1)
+        landmark_y = min(int(landmark.y * image_height), image_height - 1)
+        # landmark_z = landmark.z
+
+        landmark_point.append([landmark_x, landmark_y])
+
+    return landmark_point
+
+#Csv ve yazma
+def logging_csv(number, mode, landmark_list):
+    if mode == 0:
+        pass
+    if mode == 1 and (0 <= number <= 9):
+        csv_path = 'keypoint_classifier\keypoint.csv'
+        with open(csv_path, 'a', newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow([number, *landmark_list])
+    return
+
+#ön işleme
+def Veri_alma(landmark_list,scaler=False):
+    temp_landmark_list = copy.deepcopy(landmark_list)
+
+    #Goreli koordinatlara donustur
+    base_x, base_y = 0, 0
+    for index, landmark_point in enumerate(temp_landmark_list):
+        if index == 0:
+            base_x, base_y = landmark_point[0], landmark_point[1]
+
+        temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
+        temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
+
+    # Tek boyutlu listeye dönüştürme
+    temp_landmark_list = list(
+        itertools.chain.from_iterable(temp_landmark_list))
+    
+    # Olcekleme MinMaxscaler 
+    if scaler==True :
+        scaler_path=r"C:\Users\aliha\OneDrive\Desktop\el_algılama\keypoint_classifier\scaler.joblib"
+        scaler=load(scaler_path)
+        data = pd.DataFrame(temp_landmark_list )
+        data = data.T
+        data=scaler.transform(data)
+        test_dataset = tf.data.Dataset.from_tensor_slices(data)
+        test_dataset = test_dataset.batch(len(data))
+        
+        return test_dataset
+    
+    return temp_landmark_list 
+
+#Tahminleri Video da cizdirme
+def draw_info_text(image, brect, handedness, hand_sign_text
+                   ):
+    cv2.rectangle(image, (brect[0], brect[1]), (brect[2], brect[1] - 22),
+                 (0, 0, 0), -1)
+
+    info_text = handedness.classification[0].label[0:]
+    if hand_sign_text != "":
+        info_text = info_text + ':' + hand_sign_text
+    cv2.putText(image, info_text, (brect[0] + 5, brect[1] - 4),
+               cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, cv2.LINE_AA)
+
+    return image
+
+#El Noktarlı isaretleme ve cizme
+def draw_landmarks(image, landmark_point):
+    if len(landmark_point) > 0:
+        # Thumb
+        cv2.line(image, tuple(landmark_point[2]), tuple(landmark_point[3]),
+                (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[2]), tuple(landmark_point[3]),
+                (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[3]), tuple(landmark_point[4]),
+                (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[3]), tuple(landmark_point[4]),
+                (255, 255, 255), 2)
+
+        # Index finger
+        cv2.line(image, tuple(landmark_point[5]), tuple(landmark_point[6]),
+                (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[5]), tuple(landmark_point[6]),
+                (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[6]), tuple(landmark_point[7]),
+                (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[6]), tuple(landmark_point[7]),
+                (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[7]), tuple(landmark_point[8]),
+                (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[7]), tuple(landmark_point[8]),
+                (255, 255, 255), 2)
+
+        # Middle finger
+        cv2.line(image, tuple(landmark_point[9]), tuple(landmark_point[10]),
+                (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[9]), tuple(landmark_point[10]),
+                (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[10]), tuple(landmark_point[11]),
+                (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[10]), tuple(landmark_point[11]),
+                (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[11]), tuple(landmark_point[12]),
+                (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[11]), tuple(landmark_point[12]),
+                (255, 255, 255), 2)
+
+        # Ring finger
+        cv2.line(image, tuple(landmark_point[13]), tuple(landmark_point[14]),
+                (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[13]), tuple(landmark_point[14]),
+                (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[14]), tuple(landmark_point[15]),
+                (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[14]), tuple(landmark_point[15]),
+                (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[15]), tuple(landmark_point[16]),
+                (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[15]), tuple(landmark_point[16]),
+                (255, 255, 255), 2)
+
+        # Little finger
+        cv2.line(image, tuple(landmark_point[17]), tuple(landmark_point[18]),
+                (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[17]), tuple(landmark_point[18]),
+                (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[18]), tuple(landmark_point[19]),
+                (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[18]), tuple(landmark_point[19]),
+                (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[19]), tuple(landmark_point[20]),
+                (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[19]), tuple(landmark_point[20]),
+                (255, 255, 255), 2)
+
+        # Palm
+        cv2.line(image, tuple(landmark_point[0]), tuple(landmark_point[1]),
+                (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[0]), tuple(landmark_point[1]),
+                (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[1]), tuple(landmark_point[2]),
+                (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[1]), tuple(landmark_point[2]),
+                (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[2]), tuple(landmark_point[5]),
+                (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[2]), tuple(landmark_point[5]),
+                (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[5]), tuple(landmark_point[9]),
+                (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[5]), tuple(landmark_point[9]),
+                (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[9]), tuple(landmark_point[13]),
+                (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[9]), tuple(landmark_point[13]),
+                (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[13]), tuple(landmark_point[17]),
+                (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[13]), tuple(landmark_point[17]),
+                (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[17]), tuple(landmark_point[0]),
+                (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[17]), tuple(landmark_point[0]),
+                (255, 255, 255), 2)
+
+    # Key Points
+    for index, landmark in enumerate(landmark_point):
+        if index == 0:  # 手首1
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (255, 255, 255),
+                      -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)
+        if index == 1:  # 手首2
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (255, 255, 255),
+                      -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)
+        if index == 2:  # 親指：付け根
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (255, 255, 255),
+                      -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)
+        if index == 3:  # 親指：第1関節
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (255, 255, 255),
+                      -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)
+        if index == 4:  # 親指：指先
+            cv2.circle(image, (landmark[0], landmark[1]), 8, (255, 255, 255),
+                      -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 8, (0, 0, 0), 1)
+        if index == 5:  # 人差指：付け根
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (255, 255, 255),
+                      -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)
+        if index == 6:  # 人差指：第2関節
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (255, 255, 255),
+                      -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)
+        if index == 7:  # 人差指：第1関節
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (255, 255, 255),
+                      -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)
+        if index == 8:  # 人差指：指先
+            cv2.circle(image, (landmark[0], landmark[1]), 8, (255, 255, 255),
+                      -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 8, (0, 0, 0), 1)
+        if index == 9:  # 中指：付け根
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (255, 255, 255),
+                      -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)
+        if index == 10:  # 中指：第2関節
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (255, 255, 255),
+                      -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)
+        if index == 11:  # 中指：第1関節
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (255, 255, 255),
+                      -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)
+        if index == 12:  # 中指：指先
+            cv2.circle(image, (landmark[0], landmark[1]), 8, (255, 255, 255),
+                      -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 8, (0, 0, 0), 1)
+        if index == 13:  # 薬指：付け根
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (255, 255, 255),
+                      -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)
+        if index == 14:  # 薬指：第2関節
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (255, 255, 255),
+                      -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)
+        if index == 15:  # 薬指：第1関節
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (255, 255, 255),
+                      -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)
+        if index == 16:  # 薬指：指先
+            cv2.circle(image, (landmark[0], landmark[1]), 8, (255, 255, 255),
+                      -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 8, (0, 0, 0), 1)
+        if index == 17:  # 小指：付け根
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (255, 255, 255),
+                      -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)
+        if index == 18:  # 小指：第2関節
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (255, 255, 255),
+                      -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)
+        if index == 19:  # 小指：第1関節
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (255, 255, 255),
+                      -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)
+        if index == 20:  # 小指：指先
+            cv2.circle(image, (landmark[0], landmark[1]), 8, (255, 255, 255),
+                      -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 8, (0, 0, 0), 1)
+
+    return image
+
+#Sınırlayıcı Kutu Berlirleme
+def calc_bounding_rect(image, landmarks):
+    image_width, image_height = image.shape[1], image.shape[0]
+
+    landmark_array = np.empty((0, 2), int)
+
+    for _, landmark in enumerate(landmarks.landmark):
+        landmark_x = min(int(landmark.x * image_width), image_width - 1)
+        landmark_y = min(int(landmark.y * image_height), image_height - 1)
+
+        landmark_point = [np.array((landmark_x, landmark_y))]
+
+        landmark_array = np.append(landmark_array, landmark_point, axis=0)
+
+    x, y, w, h = cv2.boundingRect(landmark_array)
+
+    return [x, y, x + w, y + h]
+
+#Sınırlayı kutuyu cizme
+def draw_bounding_rect(use_brect, image, brect):
+    if use_brect:
+        # Outer rectangle
+        cv2.rectangle(image, (brect[0], brect[1]), (brect[2], brect[3]),
+                     (0, 0, 0), 3)
+    return image
+
+
+if __name__ == '__main__':
+    main()
